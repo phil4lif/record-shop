0a5a915132dbe64c893d8b22aed7acaedfe64a69
phil cowan <philipjcowan1@gmail.com>
2021-06-28 10:35:07 -0700

    adds storecontext and provider and usestorecontext fn

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -22,3 +22,7 @@ export const reducer = (state, action) => {
             return state;
     }
 }
+
+export function useProductReducer(initialState){
+    return useReducer(reducer, initialState);
+}
