bdef936d2a789473bba8097fc3fbac67c1c2638c
phil cowan <philipjcowan1@gmail.com>
2021-06-28 10:12:17 -0700

    creates reducers and actions for categories and products

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,24 @@
+import { UPDATE_PRODUCTS, UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from './actions';
+import { useReducer } from 'react';
+
+export const reducer = (state, action) => {
+    switch(action.type) {
+        case UPDATE_PRODUCTS:
+            return {
+                ...state,
+                products: [...action.products]
+            };
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [action.categories]
+            };
+        case UPDATE_CURRENT_CATEGORY:
+            return {
+                ...state,
+                currentCategory: action.currentCategory
+            };
+        default:
+            return state;
+    }
+}
